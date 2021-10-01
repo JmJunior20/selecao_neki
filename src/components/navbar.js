f0e374f98f37127a152b28bf8980be3ed08bba0a
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-10-01 15:37:47 -0300

    Adicionando a tela Home

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,13 +6,13 @@ function Navbar(){
     return (
         <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
             <div className="container">
-                <a href="#/" className="navbar-brand">Minhas Skills</a>
+                <a href="#/home" className="navbar-brand">Minhas Skills</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav">
-                    <NavbarItem href="#/" label="Home" />
+                    <NavbarItem href="#/home" label="Home" />
                     <NavbarItem href="#/cadastro-usuario" label="Usuários" />
                     <NavbarItem href="#/skills" label="Skills" />
                     <NavbarItem href="#/login" label="Login" />
---
+++ src/views/home.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1 className="display-3">Bem vindo!</h1>
+        <p className="lead">Esse é seu sistema de Skills.</p>
+        <hr className="my-4" />
+        <p>
+          Utilize um dos menus ou botões
+          abaixo para navegar pelo sistema.
+        </p>
+        <p className="lead">
+          <a
+            className="btn btn-primary btn-lg"
+            href="#/cadastro-usuario"
+            role="button"
+          >
+            <i className="fa fa-users"></i> Cadastrar Usuário
+          </a>
+          <a
+            className="btn btn-danger btn-lg"
+            href="https://bootswatch.com/flatly/#"
+            role="button"
+          >
+            <i className="fa fa-users"></i> Cadastrar Skill
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
